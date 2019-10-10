c80b38a6842b02950129dd5bd06317dc5c7ac0d6
李鹏阳 <liaoya1993@foxmail.com>
2019-10-11 06:15:08 +0800

    java operate demo

---
+++ java/OperateDemo.java
@@ -0,0 +1,9 @@
+class OperateDemo{
+
+    public static void main(String[] args){
+	int x = 6370;
+	x = x / 1000 * 1000;//最后结果x = 6000
+	System.out.println(x);
+	System.out.println( 5 % -2);//结果为1，只有被除数的5，别模数5的符号是有效的。
+    }
+}
