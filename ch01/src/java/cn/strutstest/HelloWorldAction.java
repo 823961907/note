9924be3a6fd9d6d512bee93843883667e6900007
823961907 <823961907@qq.com>
2019-11-14 14:19:40 +0800

    struts 3

--- ch01/src/java/cn/strutstest/HelloWorldAction.java
+++ ch01/src/java/cn/strutstest/HelloWorldAction.java
@@ -1,15 +1,25 @@
 package cn.strutstest;
 
-public class HelloWorldAction{
-       private String name;
-       public String execute() throws Exception {
-          return "success";
-       } 
-       public String getName() {
-          return name;
-       }
-       public void setName(String name) {
-          this.name = name;
-       }
+public class HelloWorldAction {
+ 
+    private String name;
+ 
+    public String execute() throws Exception {
+ 
+	System.out.println("getName:" + getName());
+ 
+	if (getName().equals("") || getName() == null) {
+	    return "error";
+	} else {
+	    return "success";
+	}
+    }
+ 
+    public String getName() {
+	return name;
+    }
+ 
+    public void setName(String name) {
+	this.name = name;
+    }
 }
-
