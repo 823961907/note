67b72b3e484f6817c935f5f65bd50a3c19a11d1b
823961907 <823961907@qq.com>
2019-12-09 07:46:38 +0800

    sql practice

---
+++ sql/sql1/Course.sql
@@ -0,0 +1,5 @@
+create table Course(CId varchar(10),Cname nvarchar(10),TId varchar(10));
+insert into Course values('01' , '语文' , '02');
+insert into Course values('02' , '数学' , '01');
+insert into Course values('03' , '英语' , '03');
+
---
+++ sql/sql1/Teacher.sql
@@ -0,0 +1,5 @@
+create table Teacher(TId varchar(10),Tname varchar(10));
+insert into Teacher values('01' , '张三');
+insert into Teacher values('02' , '李四');
+insert into Teacher values('03' , '王五');
+
